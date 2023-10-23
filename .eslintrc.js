7f7ac1b2d715cdaa89a469a0346bee4c92f2c992
nader2356 <naderabdessaied8@gmail.com>
2023-10-23 22:08:10 +0200

    setup prisma

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,21 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': [
+      'error',
+      {
+        fixToUnknown: true,
+        ignoreRestArgs: true,
+      },
+    ],
+    '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-unnecessary-condition': 'error',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'error',
+    'arrow-body-style': ['error', 'as-needed'],
+    
   },
 };
